a60d5a959e118b3b0095db99dd4d9970fae01fb1
ebh2025 <hilrs2024@gmail.com>
2026-03-09 14:29:22 +0530

    Updated jiraFetcher.js to load credentials from environment variables

---
+++ jiraFetcher.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+// Load your credentials from environment variables.
+const jiraUsername = process.env.JIRA_USERNAME;
+const jiraPassword = process.env.JIRA_PASSWORD;
+
+// Add other relevant code here. This is just an example of modifying to load credentials from env variables.
